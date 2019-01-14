63ae6dd04aff1eb02bf11fbb92f92d2c3a14a0ba
Wispmother Draugr scourge <widraugr@e2r9p2.21-school.ru>
2019-01-14 16:37:25 +0300

    fillit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 13:22:57 by widraugr          #+#    #+#             */
-/*   Updated: 2019/01/14 16:05:18 by widraugr         ###   ########.fr       */
+/*   Updated: 2019/01/14 16:36:04 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,94 +68,7 @@ static void		ft_delboard(char **board)
 }
 
 /*
-** Подсчитывает площадь квадрата. 
-*/
-/*
-static void		ft_calkarea(char **board)
-{
-	int area; 
-
-	area = 0;
-	area = ft_strlen(*board);
-	area *= area;
-}
-*/
-
-
-/*
-** Печатает лист все листы использовал для тестов
-*/
-
-void	ft_putlst(t_tetr *lst)
-{
-	t_tetr *iter;
-
-	iter = lst;
-	while (iter)
-	{
-		ft_putchar(iter->ch);
-		iter = iter->next;
-	}
-	ft_putstr("\n");
-}
-
-/*
-** Ищет лист соответствующий симаволу и возвращает его
-*/
-
-t_tetr	*ft_searchlst(t_tetr *lst, char ch)
-{
-	t_tetr	*iter;
-
-	iter = lst;
-	while (iter && iter->ch != ch)
-		iter = iter->next;
-	return (iter);
-}
-
-/*
-** Меняет содержимое листов местами
-*/
-
-void	ft_swapvallst(t_tetr *lsta, t_tetr *lstb)
-{
-	char	c;
-	int		i;
-	int		temp;
-
-	i = 0;
-	c = lsta->ch;
-	lsta->ch = lstb->ch;
-	lstb->ch = c;
-	while (i < 4)
-	{
-		temp = lsta->tetr_x[i];
-		lsta->tetr_x[i] = lstb->tetr_x[i];
-		lstb->tetr_x[i] = temp;
-		temp = lsta->tetr_y[i];
-		lsta->tetr_y[i] = lstb->tetr_y[i];
-		lstb->tetr_y[i] = temp;
-		i++;
-	}
-}
-
-/*
-** Меняет местами два элемента листа
-*/
-
-void	ft_swaplst(t_tetr *lst, char a, char b)
-{
-	t_tetr	*lsta;
-	t_tetr	*lstb;
-
-	lsta = ft_searchlst(lst, a);
-	lstb = ft_searchlst(lst, b);
-	ft_swapvallst(lsta, lstb);	
-}
-
-
-/*
-** Подсчет колличества элементов скиска, для подстановки в факториал и 
+** Подсчет колличества элементов скиска, для подстановки в факториал и
 ** нахождение всех вариантов перебора.
 */
 
@@ -171,11 +84,11 @@ static int	ft_numberlst(t_tetr *lst)
 	while (iter)
 	{
 		num++;
-		iter = iter->next;	
+		iter = iter->next;
 	}
 	return (num);
 }
-	
+
 /*
 ** Вычислкние стартового размера доски
 */
@@ -193,54 +106,7 @@ static int	ft_startsizeboard(t_tetr *lst)
 }
 
 /*
-** Вычисление факториала, для нахождения колличества возможных вариантов
-** Может и не понадобится 
-*/
-
-static int		ft_factorial(int nb)
-{
-	int i;
-	int a;
-
-	i = 1;
-	a = 1;
-	if (nb == 1)
-		return (1);
-	else	if (nb <= 0)
-		return (0);
-	else
-		while (i <= nb)
-		{
-			a = i * a;
-			i++;
-		}
-	return (a);
-}
-
-/*
-** Копирование двух досок
-*/
-
-static void	ft_copyboard(char **one, char **two, int num)
-{
-	int	i;
-	int j;
-
-	i = 0;
-	while (i < num)
-	{
-		j = 0;
-		while (j < num)
-		{
-			one[i][j] = two[i][j];
-			j++;
-		}
-		i++;
-	}
-}
-
-/*
-** Проверяет, может ли на данное место вставить тетримино 
+** Проверяет, может ли на данное место вставить тетримино
 */
 
 static int		ft_checktrak(char **board, int start_x, int start_y, t_tetr *tetr)
@@ -300,41 +166,6 @@ static void		ft_deltetr(char **board, int start_x, int start_y, t_tetr *tetr)
 	}
 }
 
-
-/*
-**  Проходимся по всей доске и пытаемся вписать Тетримино.
-**	Тетримино пока что задаю явно.
-**	#.
-**	##
-**	#.
-*/
-
-static int		ft_infillboard(char **board, t_tetr *tetr)
-{
-	int		j;
-	int		i;
-	int		sum;
-
-	sum = ft_strlen(*board);
-	j = 0;
-	i = 0;
-	while (i <= sum)
-	{
-		j = 0;
-		while (j <= sum)
-		{
-			if (ft_checktrak(board, j, i, tetr))
-			{
-				ft_infilltetr(board, j, i, tetr);	
-				return (1);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
 /*
 ** Рекурсивный перебор всех вариантов располохения тетрамино на доске
 */
@@ -350,8 +181,8 @@ static int	ft_recrunlst(char **board, t_tetr *lst)
 	n = ft_strlen(*board);
 	if (!lst)
 		return (1);
-	ft_putchar(lst->ch);
-	ft_putstr("\n");
+	//ft_putchar(lst->ch);
+	//ft_putstr("\n");
 	while (i < n)
 	{
 		j = 0;
@@ -376,44 +207,14 @@ static int	ft_recrunlst(char **board, t_tetr *lst)
 	return (0);
 }
 
-/*
-** Backtracing (Рекурися с возвратом) перебор всех вариантов.
-*/
-
-static void	ft_allvarlst(t_tetr *lst, int l, int r)
-{
-	int		i;
-	char	**board;
-
-	ft_putstr("na\n");
-	board = ft_creatboard(ft_startsizeboard(lst));
-	if (l == r)
-	{
-		ft_recrunlst(board, lst);
-	}
-	else 
-	{
-		i = l;
-		while (i <= r)
-		{
-			ft_swaplst(lst, 'A' + l, 'A' + i);
-			ft_allvarlst(lst, l + 1, r);
-			ft_swaplst(lst, 'A' + l, 'A' + i);
-			i++;
-		}
-	}
-}
-
 int		main(void)
 {
-	//char		**board;
 	t_tetr		*one; 
 	t_tetr		*two;
 	t_tetr		*three;
 	t_tetr		*four;
 	t_tetr		*five;
 	t_tetr		*six;
-	//t_tetr		*iter;
 
 	one = (t_tetr *)malloc(sizeof(t_tetr));
 	two	= (t_tetr *)malloc(sizeof(t_tetr));
@@ -490,7 +291,7 @@ int		main(void)
 	one->next = two;
 	two->next = three;
 	three->next = four;
-	four->next = six;
+	four->next = five;
 	five->next = six;
 	six->next = NULL;
 
@@ -498,211 +299,11 @@ int		main(void)
 	int sum = ft_startsizeboard(one);
 
 	board = ft_creatboard(sum);
-	//ft_allvarlst(one, 0, ft_numberlst(one) - 1);
 	while (!ft_recrunlst(board, one))
 	{
 		ft_delboard(board);
 		board = ft_creatboard(++sum);
 	}
 	ft_putboard(board);
-	ft_putstr("\n");
 	return (0);
 }
-
-/*
-** Пробегает по списку тетримино и по очереди пытается заполнит тетримино
-** на доску.
-*/
-/*
-static void	ft_runlst(t_tetr *lst)
-{
-	t_tetr	*iter;
-	int		num;
-	static int		j = 0;
-	static int		fact;
-	static int		finalnum = 10;
-	char	**board;
-	char	**finalboard;
-
-	fact = ft_factorial(ft_numberlst(lst));
-	num = ft_startsizeboard(lst);
-	board = ft_creatboard(num);
-	iter = lst;
-	while (iter)
-	{
-		if (!ft_infillboard(board, iter))
-		{
-			ft_delboard(board);
-			board = ft_creatboard(++num);
-			iter = lst;
-			ft_infillboard(board, iter);
-		}
-		iter = iter->next;
-	}
-	if (ft_strlen(*board) < finalnum)
-	{
-		//ft_putnbr(finalnum);
-		finalboard = ft_creatboard(num);
-		ft_copyboard(finalboard, board, num);
-		finalnum = ft_strlen(*board);
-	}
-	ft_putnbr(++j);
-	//j++;
-	ft_putnbr(finalnum);
-	ft_putstr("\n");
-	if (j == fact)
-	{
-		//ft_putnbr(finalnum);
-		ft_putboard(finalboard);
-		ft_putstr("\n");
-	}
-	ft_putboard(board);
-	ft_putstr("\n");
-	ft_delboard(board);
-	//board = ft_creatboard(4);
-}
-
-
-
-
-int				main(void)
-{
-	char		**board;
-	int			num;
-	t_tetr		*one; 
-	t_tetr		*two;
-	t_tetr		*three;
-	t_tetr		*four;
-	//t_tetr		*iter;
-
-	num = 4;
-	one = (t_tetr *)malloc(sizeof(t_tetr));
-	two	= (t_tetr *)malloc(sizeof(t_tetr));
-	three = (t_tetr *)malloc(sizeof(t_tetr));
-	four = (t_tetr *)malloc(sizeof(t_tetr));
-	one->ch = 'A';
-	one->tetr_x[0] = 0;
-	one->tetr_x[1] = 1;
-	one->tetr_x[2] = 0;
-	one->tetr_x[3] = 0;
-
-	one->tetr_y[0] = 0;
-	one->tetr_y[1] = 1;
-	one->tetr_y[2] = 1;
-	one->tetr_y[3] = 2;
-	
-	two->ch = 'B';
-	two->tetr_x[0] = 0;
-	two->tetr_x[1] = 0;
-	two->tetr_x[2] = 0;
-	two->tetr_x[3] = 0;
-
-	two->tetr_y[0] = 0;
-	two->tetr_y[1] = 1;
-	two->tetr_y[2] = 2;
-	two->tetr_y[3] = 3;
-
-	three->ch = 'C';
-	three->tetr_x[0] = 0;
-	three->tetr_x[1] = 1;
-	three->tetr_x[2] = 0;
-	three->tetr_x[3] = 1;
-
-	three->tetr_y[0] = 0;
-	three->tetr_y[1] = 0;
-	three->tetr_y[2] = 1;
-	three->tetr_y[3] = 1;
-
-	four->ch = 'D';
-	four->tetr_x[0] = 0;
-	four->tetr_x[1] = 1;
-	four->tetr_x[2] = 0;
-	four->tetr_x[3] = 1;
-
-	four->tetr_y[0] = 0;
-	four->tetr_y[1] = 0;
-	four->tetr_y[2] = 1;
-	four->tetr_y[3] = 1;
-
-	one->next = two;
-	two->next = three;
-	three->next = NULL;
-	//four->next = NULL;
-
-	board = ft_creatboard(num);
-	ft_allvar(board, one);
-	//ft_putnbr(ft_infillboard(board, 0, 0, one));
-	//ft_putnbr(ft_infillboard(board, 0, 0, two));
-	//ft_putnbr(ft_infillboard(board, 0, 0, three));
-	//ft_putnbr(ft_infillboard(board, 0, 0, three));
-	ft_putboard(board);
-	ft_delboard(board);
-	return (0);
-}
-*/
-/*
-** Функция меняющая два сосдних элементов списка местами
-*/
-/*
-static t_tetr	*ft_swichlst(t_tetr *tetr)
-{
-	t_tetr *second;
-	t_tetr *first;
-
-	first = tetr;
-	second = tetr->next;
-	first->next = tetr->next->next;
-	second->next = first;
-	return (second);
-}
-*/
-/*
-** Функция перебирает все возможные варианты и выподит доску в консоль
-*/
-/*
-static void		ft_allvar(char **board, t_tetr *tetr)
-{
-	int		num;
-	int		i;
-	t_tetr	*iter;
-	t_tetr	*first;
-
-	num = 4;
-	i = 1;
-	first = tetr;
-	iter = first;
-	while (i < 5)
-	{
-		ft_putnbr(i);
-		ft_putstr("l\n");
-		while (iter) 
-		{
-			if (!ft_infillboard(board, iter))
-			{
-				ft_delboard(board);
-				board = ft_creatboard(++num);
-				iter = tetr;
-				ft_infillboard(board, iter);
-			}
-			iter = iter->next;
-		}*/
-		/*if (i%2 != 0)
-		{
-			iter = ft_swichlst(first);
-			first = iter;
-		} 
-		else
-		{
-			iter = ft_swichlst(first->next);
-			first = iter;
-		}*/ 
-		/*	iter = ft_swichlst(first);
-			first = iter;
-		num = 4;
-		ft_delboard(board);
-		board = ft_creatboard(num);
-		i++;
-	}
-}
-*/
-
