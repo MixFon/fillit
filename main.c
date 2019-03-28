027c54cf97e4887d82cc758871f02c9ed275ceec
Wispmother Draugr scourge <widraugr@e3r3p1.21-school.ru>
2019-03-28 09:20:54 +0300

    fillit

--- main.c
+++ main.c
@@ -3,308 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eskeleto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/28 13:22:57 by widraugr          #+#    #+#             */
-/*   Updated: 2019/01/14 16:36:04 by widraugr         ###   ########.fr       */
+/*   Created: 2018/12/26 17:03:38 by eskeleto          #+#    #+#             */
+/*   Updated: 2019/01/17 14:19:57 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft_last_ver/libft.h"
 #include "fillit.h"
 
-/*
-** Создание новой чистой доски, размерностью nxn.
-*/
-
-static char		**ft_creatboard(size_t n)
+static void	ft_delmas(char **mas)
 {
-	char	**board;
-	size_t	i;
-
-	i = 0;
-	if (!(board = (char **)malloc(sizeof(char *) * (n + 1))))
-		return (NULL);
-	while (i < n)
+	while (*mas)
 	{
-		if (!(board[i] = (char *)malloc(sizeof(char) * (n + 1))))
-			return (NULL);
-		board[i] = (char *)ft_memset(board[i], '.', n);
-		board[i][n] = '\0';
-		i++;
+		ft_strdel(mas);
+		mas++;
 	}
-	board[i] = NULL;
-	return (board);
+	mas = NULL;
 }
 
-/*
-** Вывод доски в консоль
-*/
-
-static void		ft_putboard(char **board)
+static void	ft_dellist(t_tetr **alst)
 {
-	while (*board)
+	t_tetr	*list;
+	t_tetr	*nextlist;
+
+	if (alst == NULL)
+		return ;
+	list = *alst;
+	while (list != NULL)
 	{
-		ft_putstr(*board);
-		ft_putstr("\n");
-		board++;
+		nextlist = list->next;
+		free(list);
+		list = nextlist;
 	}
+	*alst = NULL;
 }
 
-/*
-** Удаление доски.
-** Нужна проверка на утечки, возможны утечки в цикле.
-*/
-
-static void		ft_delboard(char **board)
+t_tetr		*ft_get_list(int fd)
 {
-	while (*board)
+	char	*strfile;
+	char	**stetr;
+	int		len;
+	t_tetr	*list;
+
+	len = 0;
+	strfile = ft_read_file(fd);
+	if (!ft_is_valid_lines(strfile, &len) || len > 26)
 	{
-		ft_strdel(board);
-		board++;
+		ft_putendl("error");
+		exit(-1);
 	}
-	//free(board);
-	board = NULL;
+	stetr = ft_write_tetrimino(strfile, len);
+	free(strfile);
+	if (!ft_check_tetrimino(stetr))
+	{
+		ft_putendl("error");
+		exit(-1);
+	}
+	list = ft_coords_list(len, stetr);
+	ft_delmas(stetr);
+	free(stetr);
+	return (list);
 }
 
-/*
-** Подсчет колличества элементов скиска, для подстановки в факториал и
-** нахождение всех вариантов перебора.
-*/
-
-static int	ft_numberlst(t_tetr *lst)
+int			main(int argc, char **argv)
 {
-	t_tetr	*iter;
-	int		num;
+	int		fd;
+	t_tetr	*one;
 
-	if (!lst)
-		return (0);
-	num = 0;
-	iter = lst;
-	while (iter)
+	if (argc != 2)
 	{
-		num++;
-		iter = iter->next;
+		ft_putendl("usage ./fillit file");
+		return (-1);
 	}
-	return (num);
-}
-
-/*
-** Вычислкние стартового размера доски
-*/
-
-static int	ft_startsizeboard(t_tetr *lst)
-{
-	int	len;
-	int numberlst;
-
-	len = 2;
-	numberlst = ft_numberlst(lst);
-	while ((len * len) < (numberlst * 4))
-		len++;
-	return (len);
-}
-
-/*
-** Проверяет, может ли на данное место вставить тетримино
-*/
-
-static int		ft_checktrak(char **board, int start_x, int start_y, t_tetr *tetr)
-{
-	int		i;
-	int		sum;
-
-	sum = ft_strlen(*board);
-	i = 0;
-	if (!tetr)
-		return (0);
-	while (i < 4)
+	if ((fd = open(argv[1], O_RDONLY)) < 0)
 	{
-		if (start_y + tetr->tetr_y[i] >= sum || start_x + tetr->tetr_x[i] >= sum
-			|| start_y + tetr->tetr_y[i] < 0 || start_x + tetr->tetr_x[i] < 0)
-			return (0);
-		if (board[start_y + tetr->tetr_y[i]][start_x + tetr->tetr_x[i]] != '.')
-			return (0);
-		i++;
+		ft_putendl("error");
+		return (-1);
 	}
-	return (1);
-}
-
-/*
-** Заполняет доку тeтримино (буквами)
-** Печатал каждый раз символ который заполнял
-*/
-
-static void		ft_infilltetr(char **board, int start_x, int start_y, t_tetr *tetr)
-{
-	int i;
-
-	i = 0;
-	while (i < 4)
+	one = ft_get_list(fd);
+	ft_putfinalboard(one);
+	ft_dellist(&one);
+	if (close(fd) == -1)
 	{
-		board[start_y + tetr->tetr_y[i]][start_x + tetr->tetr_x[i]] = tetr->ch;
-		i++;
-		//ft_putboard(board);
-		//ft_putstr("\n");
+		ft_putendl("error");
+		return (-1);
 	}
-}
-
-/*
-** Удаляет с доски тетрамино.
-*/
-
-static void		ft_deltetr(char **board, int start_x, int start_y, t_tetr *tetr)
-{
-	int i;
-
-	i = 0;
-	while (i < 4)
-	{
-		board[start_y + tetr->tetr_y[i]][start_x + tetr->tetr_x[i]] = '.';
-		i++;
-		//ft_putboard(board);
-		//ft_putstr("\n");
-	}
-}
-
-/*
-** Рекурсивный перебор всех вариантов располохения тетрамино на доске
-*/
-
-static int	ft_recrunlst(char **board, t_tetr *lst)
-{
-	t_tetr	*first;
-	int		i;
-	int		j;
-	int		n;
-
-	i = 0;
-	n = ft_strlen(*board);
-	if (!lst)
-		return (1);
-	//ft_putchar(lst->ch);
-	//ft_putstr("\n");
-	while (i < n)
-	{
-		j = 0;
-		while (j < n)
-		{
-			if (ft_checktrak(board, j, i, lst))
-			{
-				ft_infilltetr(board, j, i, lst);
-				//ft_putboard(board);
-				//ft_putstr("\n");
-				if (ft_recrunlst(board, lst->next))
-					return (1);
-				ft_deltetr(board, j, i, lst);
-			}
-			j++;
-		}
-		i++;
-	}
-	//ft_putboard(board);
-	//ft_putstr("\n");
-	//ft_putstr("bab\n");
-	return (0);
-}
-
-int		main(void)
-{
-	t_tetr		*one; 
-	t_tetr		*two;
-	t_tetr		*three;
-	t_tetr		*four;
-	t_tetr		*five;
-	t_tetr		*six;
-
-	one = (t_tetr *)malloc(sizeof(t_tetr));
-	two	= (t_tetr *)malloc(sizeof(t_tetr));
-	three = (t_tetr *)malloc(sizeof(t_tetr));
-	four = (t_tetr *)malloc(sizeof(t_tetr));
-	five = (t_tetr *)malloc(sizeof(t_tetr));
-	six = (t_tetr *)malloc(sizeof(t_tetr));
-	one->ch = 'A';
-	one->tetr_x[0] = 0;
-	one->tetr_x[1] = 1;
-	one->tetr_x[2] = 0;
-	one->tetr_x[3] = 0;
-
-	one->tetr_y[0] = 0;
-	one->tetr_y[1] = 1;
-	one->tetr_y[2] = 1;
-	one->tetr_y[3] = 2;
-	
-	two->ch = 'B';
-	two->tetr_x[0] = 0;
-	two->tetr_x[1] = 0;
-	two->tetr_x[2] = 0;
-	two->tetr_x[3] = 0;
-
-	two->tetr_y[0] = 0;
-	two->tetr_y[1] = 1;
-	two->tetr_y[2] = 2;
-	two->tetr_y[3] = 3;
-
-	three->ch = 'C';
-	three->tetr_x[0] = 0;
-	three->tetr_x[1] = 1;
-	three->tetr_x[2] = 0;
-	three->tetr_x[3] = 1;
-
-	three->tetr_y[0] = 0;
-	three->tetr_y[1] = 0;
-	three->tetr_y[2] = 1;
-	three->tetr_y[3] = 1;
-
-	four->ch = 'D';
-	four->tetr_x[0] = 0;
-	four->tetr_x[1] = 1;
-	four->tetr_x[2] = 0;
-	four->tetr_x[3] = 1;
-
-	four->tetr_y[0] = 0;
-	four->tetr_y[1] = 0;
-	four->tetr_y[2] = 1;
-	four->tetr_y[3] = 1;
-
-	five->ch = 'E';
-	five->tetr_x[0] = 0;
-	five->tetr_x[1] = 1;
-	five->tetr_x[2] = 0;
-	five->tetr_x[3] = 0;
-
-	five->tetr_y[0] = 0;
-	five->tetr_y[1] = 1;
-	five->tetr_y[2] = 1;
-	five->tetr_y[3] = 2;
-	
-	six->ch = 'F';
-	six->tetr_x[0] = 0;
-	six->tetr_x[1] = 1;
-	six->tetr_x[2] = 0;
-	six->tetr_x[3] = 0;
-
-	six->tetr_y[0] = 0;
-	six->tetr_y[1] = 1;
-	six->tetr_y[2] = 1;
-	six->tetr_y[3] = 2;
-
-	one->next = two;
-	two->next = three;
-	three->next = four;
-	four->next = five;
-	five->next = six;
-	six->next = NULL;
-
-	char	**board;
-	int sum = ft_startsizeboard(one);
-
-	board = ft_creatboard(sum);
-	while (!ft_recrunlst(board, one))
-	{
-		ft_delboard(board);
-		board = ft_creatboard(++sum);
-	}
-	ft_putboard(board);
 	return (0);
 }
